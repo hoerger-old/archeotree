0656b845403702b75e0ea549d0e17cca77c52b0d
Simon Hecht <simon@Cottonmouth.(none)>
2011-07-21 03:47:55 +0200

    modified:   main/search_indexes.py 	new file:   whoosh_search_index/MAIN_READLOCK 	new file:   whoosh_search_index/MAIN_WRITELOCK 	new file:   whoosh_search_index/_MAIN_1.fln 	new file:   whoosh_search_index/_MAIN_1.pst 	new file:   whoosh_search_index/_MAIN_1.sto 	new file:   whoosh_search_index/_MAIN_1.toc 	new file:   whoosh_search_index/_MAIN_1.trm 	modified:   settings.py 	modified:   whoosh_search_index/_MAIN_1.toc

--- indianerjones/main/search_indexes.py
+++ indianerjones/main/search_indexes.py
@@ -4,18 +4,19 @@ from haystack import site
 from main.models import Student
 
 class StudentIndex(SearchIndex):
-    last_name = CharField(model_attr='last_name', document=True)
-    first_name = CharField(model_attr='first_name')
-    middle_name = CharField(model_attr='middle_name')
-    publish_date = IntegerField(model_attr='publish_date')
-    title = CharField(model_attr='title')    
-    subtitle = CharField(model_attr='subtitle')    
-    isbn = CharField(model_attr='isbn')    
-# department
-# country
-# adviser    
+   last_name = CharField(model_attr='last_name', document=True)
+   first_name = CharField(model_attr='first_name')
+   middle_name = CharField(model_attr='middle_name')
+   title = CharField(model_attr='title')    
+   publish_date = IntegerField(model_attr='publish_date')
+   subtitle = CharField(model_attr='subtitle')    
+   isbn = CharField(model_attr='isbn')
+   department = CharField(model_attr='department__name')
+   university = CharField(model_attr='department__university__name')
+   country = CharField(model_attr='department__university__country__name')
+# adviser
 
-    def index_queryset(self):
+   def index_queryset(self):
         """Used when the entire index for model is updated."""
         return Student.objects
 	#.filter(publish_date__lte=datetime.datetime.now())
