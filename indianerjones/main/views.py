b274d5c00eda115938db3a3813ad5f6d67ad6dc6
hanse <thehanse@gmail.com>
2011-07-21 12:37:11 +0200

    changes

--- indianerjones/main/views.py
+++ indianerjones/main/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from haystack import forms
+import datetime
+
+def index(request):
+    if request.method == 'GET':
+        form = forms.SearchForm(request.GET)
+        if form.is_valid():
+            result = form.search().all()    
+            return render_to_response('search.html', { 'form': form, 'res':result})
+    else:
+        form = forms.SearchForm()
+    return render_to_response('search.html', { 'form': form, })
