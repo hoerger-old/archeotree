c0cf8444f1b5ec749daa02bafde40f7f2833c6c3
hanse <thehanse@gmail.com>
2011-07-21 02:52:05 +0200

    search templates

---
+++ indianerjones/main/search_indexes.py
@@ -0,0 +1,5 @@
+import datetime
+from haystack.indexes import *
+from haystack import site
+
+from main.models import * 
--- indianerjones/settings.py
+++ indianerjones/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'indianerjones.urls'
 
 TEMPLATE_DIRS = (
+    os.path.abspath("templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
